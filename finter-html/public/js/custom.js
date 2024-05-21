0ff10a8ae774d9fbf10d890de33c42eb472d9980
soyeon <sykim0659@naver.com>
2024-05-21 22:52:45 +0900

    로그인

--- finter-html/public/js/custom.js
+++ finter-html/public/js/custom.js
@@ -81,3 +81,26 @@ function myMap() {
 $(document).ready(function () {
     $('select').niceSelect();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var loginForm = document.getElementById("loginForm");
+
+    loginForm.onsubmit = function (event) {
+        event.preventDefault(); // 기본 폼 제출 방지
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+
+        fetch('users.json')
+            .then(response => response.json())
+            .then(users => {
+                var user = users.find(user => user.username === username && user.password === password);
+                if (user) {
+                    alert('로그인 성공!');
+                } else {
+                    alert('아이디 또는 비밀번호가 잘못되었습니다.');
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    };
+});
+
